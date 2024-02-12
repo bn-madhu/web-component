5bce3618c1088b0e593349c3b99a2624c09b5849
Madhu BN <madhu.bn@mediassist.in>
2024-02-12 11:00:35 +0530

    webcomponent working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import MyWebComponent from './components/Mywebcomponent';
 import HelloWorld from './components/HelloWorld';
+import './HelloWorldWebComponent'
 
 function App(props) {
   return (
     <div className="App">
       {/* <MyWebComponent props={props} /> */}
-      <hello-world-web-component name="Madhu"></hello-world-web-component>
+      {/* <hello-world-web-component name="Madhu"></hello-world-web-component> */}
+      {/* <script src="./currentDate.js"></script> */}
+
+      <h2>Today's Date</h2>
+      The current date is: <current-date></current-date>
+      {/* <current-date name="Wc-link"></current-date> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-ReactDOM.hydrate(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
+// ReactDOM.hydrate(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
